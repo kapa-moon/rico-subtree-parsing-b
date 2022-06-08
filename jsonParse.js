26a3af68a732940f4fc39d58a2fe78209a46bb5c
Kapa <100012430+kapa-moon@users.noreply.github.com>
2022-06-08 16:03:21 -0400

    Add files via upload

---
+++ jsonParse.js
@@ -0,0 +1,47 @@
+const json = '{"focused": false, 
+"selected": false, 
+"scrollable-vertical": false, 
+"children": [{
+    "scrollable-horizontal": true, 
+    "draw": false, 
+    "ancestors": [
+      "android.webkit.WebView", 
+      "android.widget.AbsoluteLayout", 
+      "android.view.ViewGroup", 
+      "android.view.View", 
+      "java.lang.Object"
+    ], 
+    "clickable": true, 
+    "pressed": "not_pressed", 
+    "focusable": false, 
+    "long-clickable": true, 
+    "enabled": true, 
+    "bounds": [
+      1, 
+      2217, 
+      1439, 
+      2392
+    ], 
+    "visibility": "visible", 
+    "content-desc": [
+      null
+    ], 
+    "rel-bounds": [
+      0, 
+      0, 
+      1438, 
+      175
+    ], 
+    "focused": false, 
+    "selected": false, 
+    "scrollable-vertical": true, 
+    "adapter-view": false, 
+    "abs-pos": true, 
+    "pointer": "fd70e95", 
+    "class": "com.google.android.gms.ads.internal.webview.o", 
+    "visible-to-user": true
+  }
+]}';
+    
+
+const objects = JASON.parse(json);
